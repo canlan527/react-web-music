0e5fe31228844d14b1ea68b0679872c7f1ed8be9
灿烂 <1481086138@qq.com>
2023-05-21 22:25:03 +0800

    类组件+ts的学习

---
+++ src/views/demo/demo_class.tsx
@@ -0,0 +1,17 @@
+import React, { PureComponent } from 'react'
+
+class Demo2 extends PureComponent{
+  constructor(props: any) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        Demo2 - class
+      </div>
+    )
+  }
+}
+
+export default Demo2
