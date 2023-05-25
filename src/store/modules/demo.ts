86a7d246cf28a677cbd562572a9da95b814d3357
灿烂 <1481086138@qq.com>
2023-05-25 22:12:06 +0800

    类组件中ts的声明

--- src/store/modules/demo.ts
+++ src/store/modules/demo.ts
@@ -1,12 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface IState {
+  counter: number
+  direction: 'left' | 'right' | 'up' | 'down'
+}
+// 单独提取出来initialState，可以指定里面复杂变量的类型
+const initialState: IState = {
+  counter: 100,
+  direction: 'left',
+}
 
 const DemoSlice = createSlice({
-  initialState: {
-    counter: 100,
-  },
+  initialState,
   name: 'Demo',
   reducers: {
-    changeCounterAction(state, { payload }) {
+    changeCounterAction(state, { payload }: PayloadAction<number>) {
       state.counter += payload
     },
   },
--- src/views/demo/demo_class.tsx
+++ src/views/demo/demo_class.tsx
@@ -1,14 +1,54 @@
 import React, { PureComponent } from 'react'
 
-class Demo2 extends PureComponent{
-  constructor(props: any) {
-    super(props)
+interface IProps {
+  name: string
+  age: string
+}
+
+interface IState {
+  school: string
+  height: number
+}
+
+interface ISnapShot {
+  address: string
+}
+
+// 类的接口，第一个props接口，第二个State接口
+
+class Demo2 extends PureComponent<IProps, IState, ISnapShot> {
+  state = {
+    school: '一中',
+    height: 1.99,
   }
+
+  getSnapshotBeforeUpdate() {
+    return {
+      address: '瑞典',
+    }
+  }
+
+  // componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<IState>, snapshot?: ISnapShot | undefined): void {}
+
+  // constructor(props: IProps) {
+  //   super(props)
+  //   this.state = {
+  //     school: '一中',
+  //     height: 1.88,
+  //   }
+  // }
 
   render() {
     return (
       <div>
         Demo2 - class
+        {this.props.age}
+        {this.props.name}
+        {/* 不支持，可以先将state转any，但是这样很麻烦，也没有语法提示 */}
+        {/* {(this.state as any).school} */}
+        {/* 解决方法 */}
+        {this.state.height}
+        {this.state.school}
       </div>
     )
   }
