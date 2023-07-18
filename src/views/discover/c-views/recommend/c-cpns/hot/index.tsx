369af77db0a706f117a7f86a60c6ef466481ae21
灿烂 <1481086138@qq.com>
2023-07-18 19:36:13 +0800

    热门推荐布局

--- src/views/discover/c-views/recommend/c-cpns/hot/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/hot/index.tsx
@@ -2,15 +2,39 @@ import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { HotWrapper } from './style'
 import AreaHeaderV1 from '@/components/area-header-v1'
+import { useAppSelector, appShallowEqual } from '@/store/index'
 
 interface IProps {
   children?: ReactNode
 }
 
 const Hot: FC<IProps> = memo(() => {
+  // 从rtk里拿数据
+  const { hot } = useAppSelector(
+    (state) => ({
+      hot: state.recommend.hot,
+    }),
+    appShallowEqual
+  )
+
   return (
     <HotWrapper>
       <AreaHeaderV1 title="热门推荐" keylist={['华语', '民谣', '摇滚', '流行', '电子']} moreLink="/discover/songs" />
+      <div className="hot-recommend">
+        {hot.map((item) => (
+          <div className="hot-item" key={item.id}>
+            <div className="item-top">
+              <div className="item-img">
+                <i className="iconfont icon-play"></i>
+                <img src={item.picUrl} alt="" />
+              </div>
+            </div>
+            <div className="item-bottom">
+              <span className="item-title">{item.name}</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </HotWrapper>
   )
 })
--- src/views/discover/c-views/recommend/c-cpns/hot/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/hot/style.ts
@@ -1,3 +1,77 @@
 import styled from 'styled-components'
 
-export const HotWrapper = styled.div``
+export const HotWrapper = styled.div`
+  padding: 20px;
+  width: 1100px;
+  .hot-recommend {
+    ${(props) => props.theme.mixin.flexSpaceBetween};
+    align-items: flex-start;
+    flex-wrap: wrap;
+    .hot-item {
+      display: flex;
+      flex-direction: column;
+      margin: 0 20px 28px 0;
+      width: 220px;
+      transition: 0.2s ease;
+      .item-top {
+        width: 220px;
+        border-radius: 4px;
+        transition: 0.2s ease;
+        cursor: pointer;
+        .item-img {
+          position: relative;
+          width: 100%;
+          height: 220px;
+          margin-bottom: 12px;
+          overflow: hidden;
+          .iconfont {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) scale(0.3);
+            border-radius: 50%;
+            opacity: 0;
+            transition: 0.2s ease;
+            font-size: 56px;
+            color: #f5f5f5;
+          }
+          img {
+            width: 100%;
+            height: 220px;
+            transition: all 0.2s ease;
+            &:hover {
+              transform: scale(1.2);
+            }
+          }
+          &:hover {
+            .iconfont {
+              transform: translate(-50%, -50%) scale(1.2);
+              opacity: 1;
+              z-index: 9;
+            }
+          }
+        }
+
+        /* &:hover {
+          .item-img {
+            transform: scale(1.1);
+          }
+          .item-cover {
+            opacity: 1;
+            transform: scale(1.1);
+          }
+        } */
+        &:nth-child(4n) {
+          margin-right: 0;
+        }
+      }
+      .item-bottom {
+        font-size: 14px;
+        color: #424242;
+        .item-title {
+          display: flex;
+        }
+      }
+    }
+  }
+`
