4c4d8187cc47037b29ae73eef12e99b2031748aa
灿烂 <1481086138@qq.com>
2023-10-02 16:51:09 +0800

    首页footer部分完成

--- src/components/app-footer/index.tsx
+++ src/components/app-footer/index.tsx
@@ -1,12 +1,67 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
+import { AppFootterWrapper } from './style'
 
 interface IProps {
   children?: ReactNode
 }
 
 const AppFooter: FC<IProps> = () => {
-  return <div>AppFooter</div>
+  return (
+    <AppFootterWrapper>
+      <div className="app-footer-container">
+        <div className="app-footer-top">
+          <div className="app-footer-sec1">
+            <div className="sec1-title">你可以在这些平台上查看网站</div>
+            <ul className="sec1-ul">
+              <li className="sec1-item">
+                <i className="iconfont icon-pc"></i>
+                <span>PC版</span>
+              </li>
+              <li className="sec1-item">
+                <i className="iconfont icon-ios"></i>
+                <span>MAC版</span>
+              </li>
+              <li className="sec1-item">
+                <i className="iconfont icon-android"></i>
+                <span>android版</span>
+              </li>
+            </ul>
+          </div>
+          <div className="app-footer-sec1">
+            <div className="sec1-title">你可以在这些平台欣赏音乐</div>
+            <ul className="sec1-ul">
+              <li className="sec1-item">
+                <i className="iconfont icon-67"></i>
+                <span>音效</span>
+              </li>
+              <li className="sec1-item">
+                <i className="iconfont icon-zhibo-01"></i>
+                <span>直播</span>
+              </li>
+              <li className="sec1-item">
+                <i className="iconfont icon-microphone-fill"></i>
+                <span>K歌</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="app-footer-bottom">
+          <p>
+            <a>关于本站 | </a>
+            <a>&nbsp;About WebSite| </a>
+            <a>&nbsp;服务条款 | </a>
+            <a>&nbsp;用户服务协议 | </a>
+            <a>&nbsp;隐私政策 | </a>
+            <a>&nbsp;联系开发者 | </a>
+            <a href="www.github.canlan527.com">&nbsp;github</a>
+          </p>
+          <p>Copyright © 2023 Canlan527. All Rights Reserved.</p>
+          <p></p>
+        </div>
+      </div>
+    </AppFootterWrapper>
+  )
 }
 
 AppFooter.displayName = 'AppFooter'
---
+++ src/components/app-footer/style.ts
@@ -0,0 +1,66 @@
+import styled from 'styled-components'
+
+export const AppFootterWrapper = styled.div`
+  height: 300px;
+  color: grey;
+  font-size: 14px;
+  background-color: #333;
+  padding: 80px 0 0 30px;
+  box-sizing: border-box;
+  .app-footer-container {
+    width: 1080px;
+    margin: 0 auto;
+    .app-footer-top {
+      display: flex;
+      margin-bottom: 20px;
+      .app-footer-sec1 {
+        flex: 1;
+        .sec1-title {
+          font-size: 38px;
+          padding: 20px;
+        }
+        .sec1-ul {
+          display: flex;
+          justify-content: space-around;
+          .sec1-item {
+            display: flex;
+            flex-direction: column;
+            &:hover .iconfont {
+              color: ${(props) => props.theme.color.secondary};
+            }
+            .iconfont {
+              display: block;
+              font-size: 34px;
+              color: #f3f3f3;
+              margin-bottom: 12px;
+            }
+            span {
+              display: block;
+              font-size: 14px;
+            }
+          }
+        }
+      }
+      .app-footer-right {
+      }
+    }
+    .app-footer-bottom {
+      margin: 20px 0;
+      p {
+        ${(props) => props.theme.mixin.flexCenter};
+        margin: 18px 0;
+        &:hover {
+          color: ${(props) => props.theme.color.secondary};
+        }
+        a {
+          font-size: 13px;
+          color: grey;
+          cursor: pointer;
+          &:hover {
+            color: ${(props) => props.theme.color.secondary};
+          }
+        }
+      }
+    }
+  }
+`
