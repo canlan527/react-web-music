425a8f690e9ad2c9eec2ac3fdd01c86d4d1556fa
灿烂 <1481086138@qq.com>
2023-10-13 19:34:27 +0800

    player组件布局

--- src/views/player/index.tsx
+++ src/views/player/index.tsx
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-import { PlayerWrapper } from './style'
+import { PlayerWrapper, PlayerContainer } from './style'
 
 interface IProps {
   children?: ReactNode
@@ -9,13 +9,47 @@ interface IProps {
 const Player: FC<IProps> = () => {
   return (
     <PlayerWrapper>
-      Player
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
+      <div className="player-header-logo">X Muscial</div>
+      <div className="player-header-login">登录</div>
+      <div className="player-mask"></div>
+      <div className="player-bg-img"></div>
+      <PlayerContainer>
+        <div className="player_item player_header"></div>
+        <div className="player_item player_content">
+          <div className="player_left">
+            <div className="player_toolbar">
+              <div className="toolbar_button">
+                <i className="iconfont icon-shoucang"></i>
+                <span>收藏</span>
+              </div>
+              <div className="toolbar_button">
+                <i className="iconfont icon-tianjia"></i>
+                <span>添加到</span>
+              </div>
+              <div className="toolbar_button">
+                <i className="iconfont icon-shanchu"></i>
+                <span>删除</span>
+              </div>
+              <div className="toolbar_button">
+                <i className="iconfont icon-shoucang"></i>
+                <span>清空列表</span>
+              </div>
+            </div>
+            <div className="player_songlist_box">
+              <div className="player_songlist">
+                <ul className="player_songlist_header"></ul>
+                <ul className="player_songlist_content">
+                  <li className="songlist_item"></li>
+                  <li className="songlist_item"></li>
+                  <li className="songlist_item"></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="player_right"></div>
+        </div>
+        <div className="player_item player_footer"></div>
+      </PlayerContainer>
     </PlayerWrapper>
   )
 }
--- src/views/player/style.ts
+++ src/views/player/style.ts
@@ -4,5 +4,141 @@ export const PlayerWrapper = styled.div`
   position: absolute;
   width: 100%;
   height: 100vh;
-  background-color: tan;
+  color: grey;
+  background-color: #333;
+  padding: 32px 80px;
+  box-sizing: border-box;
+  .player-header-logo {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    z-index: 3;
+    font-size: 40px;
+    cursor: pointer;
+  }
+  .player-header-login {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    font-size: 20px;
+    cursor: pointer;
+  }
+  .player-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: rgba(0, 0, 0, 0.35);
+    z-index: 2;
+  }
+  .player-bg-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    display: block;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50%;
+    filter: blur(65px);
+    opacity: 0.6;
+    transform: translateZ(0);
+    background-image: url(https://y.qq.com/music/photo_new/T002R300x300M0000012rYco2WSh8e.jpg?max_age=2592000);
+    background-color: rgb(255, 255, 255);
+  }
+`
+
+export const PlayerContainer = styled.div`
+  position: relative;
+  height: 100%;
+  color: hsla(0, 0%, 88.2%, 0.8);
+  font-size: 14px;
+  margin: 0 7.6%;
+  z-index: 3;
+  .player_item {
+    position: absolute;
+    width: 100%;
+  }
+  .player_content {
+    display: flex;
+    top: 11%;
+    bottom: 18%;
+    min-height: 328px;
+    color: hsla(0, 0%, 88.2%, 0.8);
+    background: pink;
+    cursor: default;
+    .player_left {
+      flex: 1;
+      background-color: orange;
+      overflow: hidden;
+      margin-right: 50px;
+      .player_toolbar {
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        width: 100%;
+        background-color: blueviolet;
+        margin-bottom: 12px;
+        .toolbar_button {
+          background-color: transparent;
+          opacity: 0.8;
+          border: 1px solid #c9c9c9;
+          border-color: hsla(0, 0%, 100%, 0.2);
+          min-width: 122px;
+          text-align: center;
+          border-radius: 2px;
+          margin-right: 8px;
+          padding: 0 23px;
+          height: 38px;
+          line-height: 38px;
+          display: inline-block;
+          white-space: nowrap;
+          box-sizing: border-box;
+          overflow: hidden;
+          .iconfont {
+            opacity: 0.6;
+            font-size: 16px;
+            margin-right: 4px;
+          }
+          span {
+            color: #bdbdbe;
+          }
+        }
+      }
+      .player_songlist_box {
+        background-color: beige;
+        width: 100%;
+        .player_songlist {
+          height: 756px;
+          .player_songlist_header {
+            height: 46px;
+            background-color: bisque;
+            margin-bottom: 14px;
+          }
+          .player_songlist_content {
+            height: 100%;
+            overflow-y: auto;
+            overflow-x: hidden;
+            .songlist_item {
+              background-color: lightskyblue;
+              height: 40px;
+              margin-bottom: 8px;
+            }
+          }
+        }
+      }
+    }
+    .player_right {
+      width: 440px;
+      background-color: tan;
+      text-align: center;
+      line-height: 24px;
+    }
+  }
+  .player_footer {
+  }
 `
