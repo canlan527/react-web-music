481d5d7de8feb19441dac381b7c4ae72d137eb95
灿烂 <1481086138@qq.com>
2023-10-18 21:53:26 +0800

    player组件布局

--- src/views/player/index.tsx
+++ src/views/player/index.tsx
@@ -47,26 +47,98 @@ const Player: FC<IProps> = () => {
                 </ul>
                 <ul className="player_songlist_content">
                   <li className="songlist_item">
-                    <div className="col1">
-                      <div className="songlist_item_checkbox">
-                        <input type="checkbox" />
-                      </div>
-                      <div className="songlist_item_number">1</div>
+                    <div className="col1 songlist_item_checkbox">
+                      <input type="checkbox" />
                     </div>
+                    <div className="songlist_item_number">1</div>
 
-                    <div className="col2 songlist_item_songname">
-                      <div className="songlist_item_songname_left ellipsis">
-                        <a href="/" target="_blank" className="songlist_item_songname_text">
+                    <div className="col2 songlist_item_songname ellipsis">
+                      <a href="/" target="_blank" className="songlist_item_songname_mv"></a>
+                      <span className="songlist_item_songname_text ellipsis">
+                        <a href="/" target="_blank" className="">
                           乘风破浪第三季 张俪、王紫璇、于文文、张蔷、《野蔷薇》
                         </a>
-                        <a href="/" target="_blank" className="songlist_item_songname_mv">
-                          MV
+                      </span>
+                      <div className="songlist_item_songname_iconmenu">
+                        <a href="/">
+                          <i className="iconfont icon-like"></i>
+                        </a>
+                        <a href="/">
+                          <i className="iconfont icon-play_circle"></i>
+                        </a>
+                        <a href="/">
+                          <i className="iconfont icon-add_circle"></i>
                         </a>
                       </div>
+                    </div>
+                    <div className="col3 songlist_item_artist ellipsis">
+                      <a href="/" target="_blank">
+                        于文文/刘恋/赵梦/张蔷/唐诗逸
+                      </a>
+                    </div>
+                    <div className="col4 songlist_item_time">
+                      <span className="sontlist_item_time_text">3:30</span>
+                      <i className="iconfont icon-delete-circle"></i>
+                    </div>
+                  </li>
+                  <li className="songlist_item">
+                    <div className="col1 songlist_item_checkbox">
+                      <input type="checkbox" />
+                    </div>
+                    <div className="songlist_item_number">1</div>
+
+                    <div className="col2 songlist_item_songname ellipsis">
+                      <a href="/" target="_blank" className="songlist_item_songname_mv"></a>
+                      <span className="songlist_item_songname_text ellipsis">
+                        <a href="/" target="_blank" className="">
+                          乘风破浪第三季 张俪、王紫璇、于文文、张蔷、《野蔷薇》
+                        </a>
+                      </span>
                       <div className="songlist_item_songname_iconmenu">
-                        <i className="iconfont icon-like"></i>
-                        <i className="iconfont icon-play_circle"></i>
-                        <i className="iconfont icon-add_circle"></i>
+                        <a href="/">
+                          <i className="iconfont icon-like"></i>
+                        </a>
+                        <a href="/">
+                          <i className="iconfont icon-play_circle"></i>
+                        </a>
+                        <a href="/">
+                          <i className="iconfont icon-add_circle"></i>
+                        </a>
+                      </div>
+                    </div>
+                    <div className="col3 songlist_item_artist ellipsis">
+                      <a href="/" target="_blank">
+                        于文文/刘恋/赵梦/张蔷/唐诗逸
+                      </a>
+                    </div>
+                    <div className="col4 songlist_item_time">
+                      <span className="sontlist_item_time_text">3:30</span>
+                      <i className="iconfont icon-delete-circle"></i>
+                    </div>
+                  </li>
+                  <li className="songlist_item">
+                    <div className="col1 songlist_item_checkbox">
+                      <input type="checkbox" />
+                    </div>
+                    <div className="songlist_item_number">1</div>
+
+                    <div className="col2 songlist_item_songname ellipsis">
+                      <a href="/" target="_blank" className="songlist_item_songname_mv"></a>
+                      <span className="songlist_item_songname_text ellipsis">
+                        <a href="/" target="_blank" className="">
+                          乘风破浪第三季 张俪、王紫璇、于文文、张蔷、《野蔷薇》
+                        </a>
+                      </span>
+                      <div className="songlist_item_songname_iconmenu">
+                        <a href="/">
+                          <i className="iconfont icon-like"></i>
+                        </a>
+                        <a href="/">
+                          <i className="iconfont icon-play_circle"></i>
+                        </a>
+                        <a href="/">
+                          <i className="iconfont icon-add_circle"></i>
+                        </a>
                       </div>
                     </div>
                     <div className="col3 songlist_item_artist ellipsis">
