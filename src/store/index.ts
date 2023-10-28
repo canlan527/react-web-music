81eed2836f0ed424733a199d5666cf483c64419d
灿烂 <1481086138@qq.com>
2023-10-28 21:48:57 +0800

    player redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { useSelector, useDispatch, TypedUseSelectorHook, shallowEqual } from 'react-redux'
 import DemoSlice from './modules/demo'
 import recommendSlice from './modules/recommend'
+import playerSlice from './modules/player'
 
 const store = configureStore({
   reducer: {
     demo: DemoSlice,
     recommend: recommendSlice,
+    player: playerSlice
   },
 })
 export type RootType = ReturnType<typeof store.getState>
