085c8a1783401e2228341370e21028f7192d767d
灿烂 <1481086138@qq.com>
2024-02-07 14:09:19 +0800

    新歌首发部分点击播放

--- src/components/songs-item-v2/index.tsx
+++ src/components/songs-item-v2/index.tsx
@@ -2,6 +2,9 @@ import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { ItemWrapper } from './style'
 import { formatterDuration, getImageSize } from '@/utils'
+import { Link, NavLink } from 'react-router-dom'
+import { useAppDispatch } from '@/store'
+import { fetchCurrentSongAction } from '@/store/modules/player'
 
 interface IProps {
   children?: ReactNode
@@ -10,7 +13,7 @@ interface IProps {
 
 type itemType = {
   key?: string
-  id?: string
+  id: number
   picUrl?: string
   name?: string
   singerName?: string
@@ -19,14 +22,23 @@ type itemType = {
 
 const SongsItemV2: FC<IProps> = memo((props) => {
   const { item } = props
+  const dispatch = useAppDispatch()
+
+  // 点击歌曲播放
+  function handleSongPlay(id: number) {
+    dispatch(fetchCurrentSongAction(id))
+  }
+
   return (
     <ItemWrapper itemwidth="86">
       <div className="hot-item" key={item.id}>
-        <div className="item-left">
-          <div className="item-img">
-            <img src={item.picUrl && getImageSize(item.picUrl, 86)} alt="" />
-          </div>
-          <i className="iconfont icon-play"></i>
+        <div className="item-left" onClick={() => handleSongPlay(item.id)}>
+          <NavLink to={`/player/${item.id}`}>
+            <div className="item-img">
+              <img src={item.picUrl && getImageSize(item.picUrl, 86)} alt="" />
+            </div>
+            <i className="iconfont icon-play"></i>
+          </NavLink>
         </div>
         <div className="item-right">
           <div className="item-info">
