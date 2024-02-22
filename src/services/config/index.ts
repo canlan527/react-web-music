4bfa50b9ee6229fc95c143468e9097732e1a2a1d
灿烂 <1481086138@qq.com>
2024-02-22 16:31:05 +0800

    请求本地接口

--- src/services/config/index.ts
+++ src/services/config/index.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://codercba.com:9002'
+// export const BASE_URL = 'http://codercba.com:9002'
+export const BASE_URL = 'http://localhost:4000'
 export const TIME_OUT = 10000
 
 // 2. 依赖环境
