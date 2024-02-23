9b40391df1e7a0d87e682fe105a24761d803fa28
灿烂 <1481086138@qq.com>
2024-02-23 21:49:15 +0800

    完成电台页面

--- src/views/discover/c-views/radio-station/index.tsx
+++ src/views/discover/c-views/radio-station/index.tsx
@@ -1,12 +1,61 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import { appShallowEqual, useAppDispatch, useAppSelector } from '@/store'
+import { fetchRadioAction } from '@/store/modules/radio'
+
+import { Pagination } from 'antd'
+import type { PaginationProps } from 'antd'
+
+import SongsItemV1 from '@/components/songs-item-v1'
+
+import { RadioWrapper } from './style'
 
 interface IProps {
   children?: ReactNode
 }
 
 const RadioStation: FC<IProps> = () => {
-  return <div>RadioStation</div>
+  const [current, setCurrent] = useState(1)
+  // const [page, setPage] = useState(0)
+  // const [total, setTotal] = useState(0)
+
+  // 从rtk拿取数据
+  const { radiolist, limit } = useAppSelector(
+    (state) => ({
+      radiolist: state.radio.radiolist,
+      limit: state.radio.limit,
+    }),
+    appShallowEqual
+  )
+
+  // fetch数据
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchRadioAction(0))
+  }, [])
+
+  const onChange: PaginationProps['onChange'] = (page) => {
+    setCurrent(page)
+    dispatch(fetchRadioAction(page))
+    // total = page * limit 总条数 = 页数 * 每页条数
+    // const total = page * limit
+    // setTotal(total)
+  }
+
+  return (
+    <RadioWrapper>
+      <div className="section_title">热门电台</div>
+      <div className="radio_list">
+        {radiolist.map((item) => (
+          <div key={item.id} className="radio_list_item">
+            <SongsItemV1 item={item} />
+          </div>
+        ))}
+      </div>
+      <Pagination onChange={onChange} total={600} pageSize={28} />
+    </RadioWrapper>
+  )
 }
 
 RadioStation.displayName = 'RadioStation'
---
+++ src/views/discover/c-views/radio-station/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const RadioWrapper = styled.div`
+  width: 1190px;
+  margin: 0 auto;
+  .radio_list {
+    display: flex;
+    flex-wrap: wrap;
+    .radio_list_item {
+      margin: 12px 28px;
+    }
+  }
+`
