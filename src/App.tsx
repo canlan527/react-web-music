cae384e2f00ab378c48494ca2e63b7cff5ceee99
灿烂 <1481086138@qq.com>
2024-02-23 21:47:32 +0800

    设置page组件样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React, { Suspense, useRef } from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from './router'
+import { ConfigProvider } from 'antd'
 
 function App() {
   return (
     <div className="app" style={{ position: 'relative' }}>
-      <Suspense fallback={'loading'}>{useRoutes(routes)}</Suspense>
+      <ConfigProvider
+        theme={{
+          token: {
+            // Seed Token，影响范围大
+            colorPrimary: '#31C27C',
+          },
+        }}
+      >
+        <Suspense fallback={'loading'}>{useRoutes(routes)}</Suspense>
+      </ConfigProvider>
     </div>
   )
 }
