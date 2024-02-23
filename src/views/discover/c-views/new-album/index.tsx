71703ca8894159d0cc893babe416fd738bd3b855
灿烂 <1481086138@qq.com>
2024-02-23 21:49:57 +0800

    新碟上架store

--- src/views/discover/c-views/new-album/index.tsx
+++ src/views/discover/c-views/new-album/index.tsx
@@ -1,12 +1,51 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
+import { useAppDispatch, useAppSelector } from '@/store'
+import { fetchNewAlbumListAction, fetchAllNewAlbumListAction } from '@/store/modules/newAlbum'
+
+import { Pagination } from 'antd'
+import type { PaginationProps } from 'antd'
+
+import { NewAlbumWrapper } from './style'
+import NewAlbumItem from '@/components/new-album-item'
 
 interface IProps {
   children?: ReactNode
 }
 
 const NewAlbum: FC<IProps> = () => {
-  return <div>NewAlbum</div>
+  const [current, setCurrent] = useState(1)
+  // 获取dispatch
+  const dispatch = useAppDispatch()
+
+  // fetch请求
+  useEffect(() => {
+    dispatch(fetchAllNewAlbumListAction(0))
+  }, [])
+
+  // 从rtk里获取数据
+  const { newAlbumList } = useAppSelector((state) => ({
+    newAlbumList: state.newAlbum.newAlbumList,
+  }))
+
+  const onChange: PaginationProps['onChange'] = (page) => {
+    setCurrent(page)
+    dispatch(fetchAllNewAlbumListAction(page))
+  }
+
+  return (
+    <NewAlbumWrapper>
+      <div className="section_title">新碟上架</div>
+      <div className="new_album_container">
+        {newAlbumList.map((item) => (
+          <div key={item.id} className="new_album_item">
+            <NewAlbumItem item={item} />
+          </div>
+        ))}
+      </div>
+      <Pagination onChange={onChange} total={500} pageSize={35} />
+    </NewAlbumWrapper>
+  )
 }
 
 NewAlbum.displayName = 'NewAlbum'
